a028a6f24dafef2f270fec98b29e9ff04ace0921
sujin999 <sujin.herofactory@gmail.com>
2023-12-14 16:26:05 +0900

    Load->Add변경

--- src/js/ui/template/controls.js
+++ src/js/ui/template/controls.js
@@ -10,7 +10,7 @@ export default ({ locale, biImage, loadButtonStyle, downloadButtonStyle, menuBar
 
         <div class="tui-image-editor-controls-buttons">
             <div style="${loadButtonStyle}">
-                ${locale.localize('Load')}
+                ${locale.localize('Add')}
                 <input type="file" class="tui-image-editor-load-btn" />
             </div>
             <button class="tui-image-editor-download-btn" style="${downloadButtonStyle}">
